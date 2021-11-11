bced51e40f1a192d39574d67c361247e924345ce
Valeria Aguilar <valeriaaguilar@live.com>
2021-11-11 09:12:29 -0800

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
 use employee_tracker_db;
-insert into department (id, department_name)
+insert into department (department_name)
   VALUES 
-    (1, "Sales"),
-    (2, "Engineering"),
-    (3, "Legal"),
-    (4, "Finance");
+    ("Sales"),
+    ("Engineering"),
+    ("Legal"),
+    ("Finance");
 
-insert into roles (job_title, id, department_id, salary)
+insert into roles (title, department_id, salary)
   VALUES 
-    ("Lawyer", 10, 3, 110000),
-    ("Salesperson", 11, 1, 90000),
-    ("Lead Engineer", 12, 2, 105000),
-    ("Software Engineer", 13, 2, 125000),
-    ("Accountant", 14, 4, 85000),
-    ("Account Manager", 15, 4, 78000);
+    ("Lawyer", 3, 110000),
+    ("Salesperson", 1, 90000),
+    ("Lead Engineer", 2, 105000),
+    ("Software Engineer", 2, 125000),
+    ("Accountant", 4, 85000),
+    ("Account Manager", 4, 78000);
 
-insert into employee (id, first_name, last_name, roles_id, manager_id)
+insert into employee (first_name, last_name, roles_id, manager_id)
   VALUES 
-    (1, "Gary", "Cole", 12, null), 
-    (2, "Justin", "Rogers", 14, 3),
-    (3, "Wesley", "James", 15, null),
-    (4, "Edwin", "Portillo", 14, 3),
-    (5, "Renee", "Gamez", 12, null),
-    (6, "Neli", "Roses", 10, null),
-    (7, "Jesus", "Vazquez", 11, 1),
-    (8, "Sophia", "Aguilar", 13, 1),
-    (9, "Leo", "Romero", 13, 5);
+    ( "Gary", "Cole", 6, null), 
+    ( "Justin", "Rogers", 6, null),
+    ( "Wesley", "James", 5, null),
+    ( "Edwin", "Portillo", 4, 3),
+    ( "Renee", "Gamez", 2, null),
+    ( "Neli", "Roses", 1, null),
+    ( "Jesus", "Vazquez", 2, 1),
+    ( "Sophia", "Aguilar", 3, 1),
+    ( "Leo", "Romero", 3, 5);
 
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //---------------DEPENDENCIES---------------//
 const art = require("ascii-art");
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
+const mysql = require("mysql");
+const { last } = require("rxjs");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -60,106 +61,135 @@ async function options(){
 //---------------VIEW DEPTS---------------// 
 async function viewDepts(){
   console.log("TODO: You chose 'View Depts'");
-   db.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary;", function(err, res){
-   console.tables(res);
-   start();
+  
+   db.query("SELECT * from Department", function(err, res){
+   console.table(res);
+   options();
    })
 };
 //---------------VIEW ROLES---------------//
 async function viewRoles(){
-  db.query("SELECT roles.id, roles.salary, department.department_name AS department FROM roles LEFT JOIN department ON roles.department_id = department.id;", function(err, res) {
+  db.query("SELECT roles.id, roles.title, roles.salary, department.department_name AS department FROM roles LEFT JOIN department ON roles.department_id = department.id;", function(err, res) {
     console.table(res);
-    start();
+    options();
   })
 }
 
 //---------------VIEW EMPLOYEES---------------//
 async function viewEmployees(){
-  db.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name AS department, roles.salary, CONCAT(manager_employee.first_name, ' ', manager_employee.last_name) AS manager FROM employee INNER JOIN roles ON employee.role_id = roles.id INNER JOIN department ON roles.department_id = department.id LEFT JOIN employee manager_employee ON manager_employee.id = employee.manager_id ORDER BY employee.id;", function (err, res){
+  db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name AS department, roles.salary, CONCAT(manager_employee.first_name, ' ', manager_employee.last_name) AS manager 
+  FROM employee 
+  LEFT JOIN roles ON employee.roles_id = roles.id 
+  LEFT JOIN department ON roles.department_id = department.id 
+  LEFT JOIN employee manager_employee ON manager_employee.id = employee.manager_id 
+  ORDER BY employee.id;`, function (err, res){
+    console.log(res)
     console.table(res)
-    start();
+    options();
   })
 
 }
 // add a dept
 async function addDept(){
-  console.log("TODO: You chose 'View Trips'");
+  const { department_name } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the new Department Name:",
+      name: "department_name"
+    }])
+    db.query("insert into department set ?", {department_name}, function (err, res){
+      viewDepts();
+
+    })
 }
 // add a role
 async function addRole(){
-  console.log("TODO: You chose 'View Trips'");
+  db.query("SELECT * FROM department", async function (err, res){
+    let listOfDepts = res.map(({id,department_name})=>{
+      return {
+        name: department_name, value: id
+      }
+    })
+   const { title, salary, department_id } = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the title of your new Role:",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "What is the Salary:",
+        name: "salary"
+      },
+      {
+        type: "list",
+        message: "Which department does this role belong to:",
+        name: "department_id",
+        choices: listOfDepts
+      }
+    ])
+    db.query("insert into Roles set ?", { title, salary, department_id }, function (err, res){
+      viewRoles();
+    })
+  })
+  
+  //do a query to select all departemnts
+  //`deparmentArray`map the resonse so I can ask which deparment this new role  belongs too
+  //ask user for role name, salaray and department name() by using list and separment name as choices
 }
 // add an employee
 async function addEmoloyee(){
-  console.log("TODO: You chose 'View Trips'");
+  db.query("SELECT * FROM roles", async function (err, res){
+    let listOfRoles = res.map(({id, title})=>{
+      return {
+        name: title, value: id
+      }
+    })
+    db.query("SELECT * FROM employee", async function (err, res){
+      let listOfManagers = res.map(({id, first_name, last_name})=>{
+        return {
+          name: `${first_name} ${last_name}` , value: id
+        }
+      })
+      const { first_name, last_name, manager_id, roles_id } = await inquirer.prompt([
+        {
+          type: "input",
+          message: "What is your first name:",
+          name: "first_name"
+        },
+        {
+          type: "input",
+          message: "What is your last name:",
+          name: "last_name"
+        },
+        {
+          type: "list",
+          message: "Who is your manager:",
+          name: "manager_id",
+          choices: listOfManagers
+        },
+        {
+          type: "list",
+          message: "Who is is the role of the employee:",
+          name: "roles_id",
+          choices: listOfRoles
+        }
+      ])
+      db.query("insert into Employee set ?", { first_name, last_name, manager_id, roles_id }, function (err, res){
+        viewEmployees();
+      })
+  })
+})
 }
 // update an emplyee
 async function updateEmployee(){
-  console.log("TODO: You chose 'View Trips'");
-}
-// exit()
-
-
-
-
-// ---- function 2 use --- //
-/*async function updateTrip() {
-  try {
-    const selectTripsSql = `select id, destination, 
-    concat(
-      year(trip_date),
-      "-",
-      month(trip_date), 
-      "-",
-      day(trip_date)
-    ) as date from trips;`;
-
-    const [rows] = await db.promise().query(selectTripsSql);
-
-    // Create array of objects for inquirer choices. Each element is an object
-    // with { name: "Description for user", value: trip }
-    const choices = rows.map((trip) => ({
-      name: `${trip.destination} - ${trip.date}`,
-      value: trip,
-    }));
-
-    // This will be a trip object from the value property in the choices.
-    const { trip } = await inquirer.prompt([
-      {
-        type: "list",
-        message: "Choose a trip to update:",
-        name: "trip",
-        choices,
-      },
-    ]);
-    const { date, destination } = await inquirer.prompt([
-      {
-        type: "input",
-        message: "Enter destination",
-        name: "destination",
-        default: trip.destination,
-      },
-      {
-        type: "input",
-        message: "Enter trip date (e.g. 2022-03-01)",
-        name: "date",
-        default: trip.date,
-      },
-    ]);
-
-    const updateTripsSql = `UPDATE trips SET ? WHERE ?;`;
-    await db
-      .promise()
-      .query(updateTripsSql, [
-        { trip_date: date, destination },
-        { id: trip.id },
-      ]);
-    console.log("Update success.");
-    return menu();
-  } catch (error) {
-    console.log(error);
+  db.query("SELECT * FROM employee", async function (err, res){
+    let listOfEmployees = res.map(({id, title})=>{
+      return {
+        name: title, value: id
+      }
+    })
   }
-} 
-*/
+)}
 
 
